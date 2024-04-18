157bad7890f0c666b5ce35f14f72d0e65b38542c
B-Thibaut <55447253+B-Thibaut@users.noreply.github.com>
2024-04-18 12:30:59 +0200

    fin premiere session

--- full_adder.vhd
+++ full_adder.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 -- DESCRIPTION DES ENTREES/SORTIES DE L'ENTITY
-entity half_adder is
+entity full_adder is
 	port (
 		A : in std_logic;
 		B : in std_logic;
@@ -11,14 +11,22 @@ entity half_adder is
 		C : out std_logic;
 		cin : in std_logic
 	);
-end half_adder;
+end full_adder;
 
 -- DESCRIPTION COMPORTEMENTALE DE L'ENTITY
-architecture behavioral of half_adder is
+architecture behavioral of full_adder is
 -- (Optionnel) Ecrire ici la déclaration de signaux
 -- ex. signal X : std_logic;
 begin
     -- Ecrire ici les instructions cocurrentes décrivant le comportement de l'entity
-	S <= (A XOR B XOR cin);
-	C <= ((A XOR B) AND cin) OR (A  ;
+	 HA1 : entity work.half_adder port map(
+		--a definir
+	 );
+	 
+	 HA2 : entity work.half_adder port map(
+		--a definir
+	 );
+	 
+	S <= ;
+	C <= ;
 end behavioral;
